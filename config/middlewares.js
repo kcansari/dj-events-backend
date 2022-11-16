fc801663644f935b9e4c145cd1a2c766bd06dd0e
kcansari <kcansari0@gmail.com>
2022-11-16 21:10:44 +0300

    headers allow for anyone

--- config/middlewares.js
+++ config/middlewares.js
@@ -29,11 +29,8 @@ module.exports = [
   {
     name: "strapi::cors",
     config: {
-      origin: "*",
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-      credentials: false,
+      headers: "*",
     },
   },
   "strapi::poweredBy",
