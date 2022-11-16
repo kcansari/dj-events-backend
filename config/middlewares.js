586dddcdb02490adc3fb7aa9972604d9b47fecb8
kcansari <kcansari0@gmail.com>
2022-11-16 18:47:44 +0300

    Add- cors config

--- config/middlewares.js
+++ config/middlewares.js
@@ -26,8 +26,15 @@ module.exports = [
       },
     },
   },
-  "strapi::cors",
   "strapi::poweredBy",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: "*",
+    },
+  },
   "strapi::logger",
   "strapi::query",
   "strapi::body",
